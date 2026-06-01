0585638f64053666b50d23735ec61a0e45fc0a81
nematatu <nematatu1003@gmail.com>
2026-06-01 09:29:42 +0900

    fix: vite load err

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,4 @@
+import { fileURLToPath } from "node:url";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
@@ -8,12 +9,30 @@ import remarkDirective from "remark-directive";
 import rehypeImageCaption from "./src/lib/markdown/rehype-image-caption.js";
 import remarkTwitterCard from "./src/lib/markdown/remark-twitter-card.js";
 
+const viteEnvEntry = fileURLToPath(
+	new URL(
+		"./node_modules/.pnpm/node_modules/vite/dist/client/env.mjs",
+		import.meta.url,
+	),
+);
+
 // https://astro.build/config
 export default defineConfig({
 	site: "https://blog.amatatu.com",
 	integrations: [sitemap(), mdx(), pagefind(), react()],
 	vite: {
-		plugins: [tailwindcss()],
+		plugins: [
+			{
+				name: "resolve-vite-env-client",
+				enforce: "pre",
+				resolveId(id) {
+					if (id === "@vite/env") {
+						return viteEnvEntry;
+					}
+				},
+			},
+			tailwindcss(),
+		],
 	},
 	markdown: {
 		shikiConfig: {
