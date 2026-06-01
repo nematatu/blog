bf5d17012db823ae9bcc53a8b25f7fac094e53af
nematatu <nematatu1003@gmail.com>
2026-06-01 09:36:18 +0900

    fix: hot reload

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,3 @@
-import { fileURLToPath } from "node:url";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
@@ -9,30 +8,12 @@ import remarkDirective from "remark-directive";
 import rehypeImageCaption from "./src/lib/markdown/rehype-image-caption.js";
 import remarkTwitterCard from "./src/lib/markdown/remark-twitter-card.js";
 
-const viteEnvEntry = fileURLToPath(
-	new URL(
-		"./node_modules/.pnpm/node_modules/vite/dist/client/env.mjs",
-		import.meta.url,
-	),
-);
-
 // https://astro.build/config
 export default defineConfig({
 	site: "https://blog.amatatu.com",
 	integrations: [sitemap(), mdx(), pagefind(), react()],
 	vite: {
-		plugins: [
-			{
-				name: "resolve-vite-env-client",
-				enforce: "pre",
-				resolveId(id) {
-					if (id === "@vite/env") {
-						return viteEnvEntry;
-					}
-				},
-			},
-			tailwindcss(),
-		],
+		plugins: [tailwindcss()],
 	},
 	markdown: {
 		shikiConfig: {
